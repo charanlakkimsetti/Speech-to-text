317dcd58914bb50a6344309ef7839c17c79b3b63
charanlakkimsetti <144098585+charanlakkimsetti@users.noreply.github.com>
2025-08-10 16:16:04 +0530

    Add files via upload

---
+++ speech-to-text-converter-main/languages-option.js
@@ -0,0 +1,44 @@
+const languages = [
+  {
+    no: "1",
+    name: "English",
+    native: "English",
+    code: "en",
+  },
+  {
+    no: "2",
+    name: "Hindi",
+    native: "हिन्दी",
+    code: "hi",
+  },
+  {
+    no: "3",
+    name: "Telugu",
+    native: "తెలుగు",
+    code: "te",
+  },
+  {
+    no: "4",
+    name: "Chinese (Simplified)",
+    native: "中文简体",
+    code: "zh-CN",
+  },
+//   {
+//     no: "5",
+//     name: "French",
+//     native: "Français",
+//     code: "fr-FR",
+// },
+// {
+//     no: "6",
+//     name: "German",
+//     native: "Deutsch",
+//     code: "de-DE",
+// },
+// {
+//   no: "7",
+//   name: "Urdu",
+//   native: "اردو",
+//   code: "ur-PK",
+// }
+];
